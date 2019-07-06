5c0ec6836be892355b7f8ef27863ab63fdd3c063
Ian Ditsch <IanDitsch@Outlook.com>
2019-07-05 22:44:46 -0500

    Coding From Scratch JS Add Strings

---
+++ script.js
@@ -0,0 +1,7 @@
+function addStrings (string1, string2)
+{
+    return string1 + string2;
+}
+
+var concatenatedStrings = addStrings ("This is how ", "you do concatenation!"); 
+console.log(concatenatedStrings);
